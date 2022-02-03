294f81d63b6a6d151c88f78362bf7d8e33788fb3
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2022-02-03 21:35:54 +0200

    HW7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,53 +1,69 @@
 package com.company;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
-    public static void main (String[] args) {
-        HW7 exp = new HW7();
-        exp.controller(exp.pairOne, exp.pairTwo,0);
+    public static void main(String[] args) {
+        HW7 exp = new HW7(new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20});
+        exp.controller(exp.pairOne, exp.pairTwo, 0);
     }
 }
 
 class HW7 {
-    int[] arr = {2, 1, -3, 0 };
+    int[] arr;
     LinkedList<Integer> pairOne, pairTwo;
 
-    private boolean eqSum (LinkedList<Integer> A, LinkedList<Integer> B) {
-        int sum1 = 0, sum2 = 0;
-        for (int i = 0; i < A.size(); i++) {
-            sum1 += A.get(i);
+    public HW7 (int[] arr) {
+        this.arr = arr;
+    }
+
+    private boolean eqSum(LinkedList<Integer> A, LinkedList<Integer> B) {
+        int sum1 = 0;
+        if (A != null) {
+            for (int i = 0; i < A.size(); i++) {
+                sum1 = sum1 + A.get(i);
+            }
         }
-        for (int i = 0; B != null && i < B.size(); i++) {
-            sum1 += B.get(i);
+        if (B!= null) {
+            for (int i = 0; i < B.size(); i++) {
+                sum1 = sum1 - B.get(i);
+            }
         }
-        if (sum1 == sum2)
+        if (sum1 == 0) {
             return true;
+        }
         return false;
     }
 
-    public void controller (LinkedList<Integer> first, LinkedList<Integer> second, int counter) {
+    public void controller(LinkedList<Integer> first, LinkedList<Integer> second, int counter) {
         LinkedList<Integer> Anew = new LinkedList<>(), Bnew = new LinkedList<>();
         if (counter == arr.length && eqSum(first, second)) {
             outPut(first, second);
         }
         if (counter < arr.length) {
+            if (first != null)
+                Anew = (LinkedList<Integer>) first.clone();
+            if (second != null)
+                Bnew = (LinkedList<Integer>) second.clone();
             Anew.add(arr[counter]);
             Bnew.add(arr[counter]);
-            controller(Anew, second, counter++);
-            controller(first, Bnew, counter++);
+            controller(Anew, second, counter + 1);
+            controller(first, Bnew, counter + 1);
         }
     }
 
     private void outPut(LinkedList<Integer> A, LinkedList<Integer> B) {
         System.out.print("A=[");
-        for (Integer v: A) {
-            System.out.print(v);
+        if (A != null) {
+            for (Integer v : A) {
+                System.out.print(v + " ");
+            }
         }
         System.out.print("], B=[");
-        for (Integer v: A) {
-            System.out.print(v);
+        if (B != null) {
+            for (Integer v : B) {
+                System.out.print(v + " ");
+            }
         }
         System.out.print("]" + "\n");
     }
