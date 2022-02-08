7f8db8b14b4154ab90964b6a8e130aadce6689ae
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2022-02-08 10:59:19 +0200

    HW7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,70 +1,47 @@
 package com.company;
 
-import java.util.LinkedList;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
-        HW7 exp = new HW7(new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20});
-        exp.controller(exp.pairOne, exp.pairTwo, 0);
+        HW1 exp = new HW1(1000, 400);
+        exp.controller();
     }
 }
 
-class HW7 {
-    int[] arr;
-    LinkedList<Integer> pairOne, pairTwo;
+class HW1 {
+    private int x, y;
+    final String[] options = {"First", "Second", "Third"};
+    final JComboBox<String> menu = new JComboBox<>(options);
+    final JFrame frame = new JFrame("Very nice HW");
 
-    public HW7 (int[] arr) {
-        this.arr = arr;
+    public HW1(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    private boolean eqSum(LinkedList<Integer> A, LinkedList<Integer> B) {
-        int sum1 = 0;
-        if (A != null) {
-            for (int i = 0; i < A.size(); i++) {
-                sum1 = sum1 + A.get(i);
-            }
-        }
-        if (B!= null) {
-            for (int i = 0; i < B.size(); i++) {
-                sum1 = sum1 - B.get(i);
-            }
-        }
-        if (sum1 == 0) {
-            return true;
-        }
-        return false;
+    private void setCanvas(int width, int height) {
+        frame.setSize(width, height);
+        menu.setBounds(20, 20, 400, 100);
+        frame.add(menu);
+        frame.setVisible(true);
     }
 
-    public void controller(LinkedList<Integer> first, LinkedList<Integer> second, int counter) {
-        LinkedList<Integer> Anew = new LinkedList<>(), Bnew = new LinkedList<>();
-        if (counter == arr.length && eqSum(first, second)) {
-            outPut(first, second);
-        }
-        if (counter < arr.length) {
-            if (first != null)
-                Anew = (LinkedList<Integer>) first.clone();
-            if (second != null)
-                Bnew = (LinkedList<Integer>) second.clone();
-            Anew.add(arr[counter]);
-            Bnew.add(arr[counter]);
-            controller(Anew, second, counter + 1);
-            controller(first, Bnew, counter + 1);
-        }
+    public void controller() {
+        setCanvas(x, y);
+        selection();
     }
 
-    private void outPut(LinkedList<Integer> A, LinkedList<Integer> B) {
-        System.out.print("A=[");
-        if (A != null) {
-            for (Integer v : A) {
-                System.out.print(v + " ");
-            }
-        }
-        System.out.print("], B=[");
-        if (B != null) {
-            for (Integer v : B) {
-                System.out.print(v + " ");
-            }
-        }
-        System.out.print("]" + "\n");
+    public void selection() {
+        menu.addActionListener((ActionEvent e) -> {
+            Object selection = menu.getSelectedItem();
+            assert selection != null;
+            if (selection.equals("First"))
+                System.out.println("First");
+            else if (selection.equals("Second"))
+                System.out.println("Second");
+            else System.out.println("Third");
+        });
     }
 }
