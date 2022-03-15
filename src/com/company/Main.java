baa2e6b698df76517946bead96293e912e468900
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2022-03-15 11:40:46 +0200

    Homeworks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,81 @@
 package com.company;
+import ibadts.*;
+
+/* public class Main {
+    public static void main(String[] args) {
+        IntQueue queue = new IntQueue(5);
+        for (int i = 0; i < 5; i++)
+            queue.enqueue(i);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        queue.enqueue(69);
+        for (int i = 0; i < 4; i ++)
+            System.out.println(queue.dequeue());
+    }
+}
+
+class IntQueue {
+    int[] arr;
+    int capacity;
+    int count = 0;
+    public IntQueue(int capacity) {
+        arr = new int[capacity];
+        this.capacity = capacity;
+    }
+    public void enqueue (int item) {
+        if (count < capacity) {
+            arr[count] = item;
+            count++;
+        }
+        else
+            System.out.println("Limit exceeded");
+    }
+
+    public int dequeue() {
+        if (!isEmpty()) {
+            int value = arr[0];
+            for (int i = 0; i < count - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            count--;
+            return value;
+        } else return -1;
+    }
+
+    public boolean isEmpty() {
+        if (count == 0)
+            return true;
+        return false;
+    }
+
+    public boolean isFull() {
+        if (count == capacity - 1)
+            return true;
+        return false;
+    }
+} */
 
 public class Main {
     public static void main(String[] args) {
-        IntStack test = new IntStack(100);
-        for (int i = 1; !test.isFull(); i++)
-            test.push(i);
-        while (!test.isEmpty())
-            System.out.println(test.pop());
-    }
-
-}
-
-class IntStack {
-    private final int SIZE = 1000;
-    private int capacity = 0;
-    private int counter = 0;
-    private int[] staticStack = new int[1000];
-    public IntStack(int capacity) {
-        if (capacity <= SIZE)
-            this.capacity = capacity;
-        else {
-            System.out.println("Capacity too large");
-            System.exit(1);
+        Queue<String> economy = new Queue<>(), business = new Queue<>();
+        economy.enqueue("Sarah_A");
+        economy.enqueue("John_B");
+        economy.enqueue("Wendy_C");
+        business.enqueue("Sir_A");
+        business.enqueue("Madam_B");
+        business.enqueue("Rick_C");
+        byte counter = 1;
+        System.out.println("economy: " + economy);
+        System.out.println("business: " + business);
+        System.out.print("order: ");
+        while (!economy.isEmpty() || !business.isEmpty()) {
+            if (counter % 3 != 0 && !business.isEmpty())
+                System.out.print(business.dequeue() + " ");
+            else {
+                System.out.print(economy.dequeue() + " ");
+                counter = 0;
+            }
+            counter++;
         }
-    }
-    public boolean isFull() {
-        if (counter == capacity)
-            return true;
-        return false;
-    }
-    public boolean isEmpty() {
-        if (counter == 0)
-            return true;
-        return false;
-    }
-    public void push (int item) {
-        if (counter <= capacity) {
-        staticStack[counter] = item;
-        counter ++; }
-    }
-    public int pop() {
-        counter--;
-        return staticStack[counter];
     }
 }
